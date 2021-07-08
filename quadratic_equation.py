48db0dc438297f497e5e3d0afb2282b49e4fe398
YLZ <ylizhu1116@163.com>
2021-07-08 10:07:35 +0800

    可运行的一元二次求解

---
+++ quadratic_equation.py
@@ -0,0 +1,23 @@
+import math
+
+
+def quadratic(a, b, c):
+    m = b**2-4*a*c
+    if m > 0:
+        x1 = (-b-math.sqrt(m))/(2*a)
+        x2 = (-b+math.sqrt(m))/(2*a)
+        return x1, x2
+    elif m == 0:
+        x = -b/(2*a)
+        return x
+    elif m < 0:
+        print('无实解')
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0) and quadratic(2, 3, 1) != (-1.0, -0.5):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0) and quadratic(1, 3, -4) != (-4.0, 1.0):
+    print('测试失败')
+else:
+    print('测试成功')
